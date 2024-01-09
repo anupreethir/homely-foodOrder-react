9cf7931ba7a24d8e1300af3f5133fa773f28ba93
Anupreeti <anupreethirr@gmail.com>
2024-01-09 12:03:20 +0530

    clear cart issue resolved

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -15,9 +15,10 @@ const Cart = () => {
         <div className="">
             <div className="flex justify-between w-[95%] mx-auto items-center mt-[2%]">
                 <h1 className="font-medium ">Cart</h1>
-                <div className="flex items-center w-fit px-[7px] py-[2px] m-2 shadow-sm rounded-lg border-[0.01px] border-[#00000033] hover:bg-black hover:text-white">
+                <div className="flex items-center w-fit px-[7px] py-[2px] m-2 shadow-sm rounded-lg border-[0.01px] border-[#00000033] hover:bg-black hover:text-white"
+                    onClick={handleClearCart}>
                     <MdOutlineCancel className="mr-1" />
-                    <button className="  " onClick={handleClearCart}>Clear cart</button>
+                    <button className="  ">Clear cart</button>
                 </div>
             </div>
             <RestaurantCategoryList data={cartItems} />
